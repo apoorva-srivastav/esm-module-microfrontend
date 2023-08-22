e390dd1dadaf0c4578036fedf53de2aaeeb10ff8
Apoorva2696 <apoorva2696@gmail.com>
2023-08-22 16:42:37 +0530

    pnpm package

--- packages/ui/Button/Button.jsx
+++ packages/ui/Button/Button.jsx
@@ -1,4 +1,4 @@
-
+import React from 'react'
 import styles from './Button.module.css'
 /**
  * Renders a basic button component.
---
+++ packages/ui/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+
+// component exports
+export * from "./Button/Button";
+export * from "./Header/Header";
+export * from "./Footer/Footer";

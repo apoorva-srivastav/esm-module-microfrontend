ba1caad6b1c1b91458aa42d484530bf4448ee2c7
Apoorva2696 <apoorva2696@gmail.com>
2023-08-22 18:28:35 +0530

    simple react app

--- packages/ui/index.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-
-// component exports
-export * from "./Button/Button";
-export * from "./Header/Header";
-export * from "./Footer/Footer";
---
+++ packages/ui/src/index.js
@@ -0,0 +1,6 @@
+import * as React from "react";
+export { Footer } from './Footer/Footer';
+export { Button } from './Button/Button';
+export { NewTabLink } from "./NewTabLink";
+export { CounterButton } from "./CounterButton";
+export {Header} from './Header/Header'
